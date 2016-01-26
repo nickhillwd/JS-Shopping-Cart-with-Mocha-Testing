0fcc5bc6b52efafe6996fd1d7724cdd8b5c74616
nickhillwd <nickhillwd@gmail.com>
2016-01-26 16:10:59 +0000

    item adding to shopping cart test green

--- ShoppingCart.js
+++ ShoppingCart.js
@@ -1,7 +1,10 @@
 var basketItems = require('./BasketItems');
 
 var shoppingCart = {
-  itemCount: 0
+  itemCount: 0,
+  addItem: function(quantity){
+    this.itemCount += quantity
+  }
 }
 
 module.exports = shoppingCart;
